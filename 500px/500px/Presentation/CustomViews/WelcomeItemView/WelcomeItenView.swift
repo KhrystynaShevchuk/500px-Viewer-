69a599195433f9713259d67a5ae133a6f208e250
KhrystynaShevchuk <khrystyna.shevchuk94@gmail.com>
2016-05-27 16:44:39 +0300

    - config scroll view

---
+++ 500px/500px/Presentation/CustomViews/WelcomeItemView/WelcomeItenView.swift
@@ -0,0 +1,21 @@
+//
+//  WelcomeItenView.swift
+//  500px
+//
+//  Created by KhrystynaShevchuk on 5/27/16.
+//  Copyright © 2016 KhrystynaShevchuk. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeItenView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
