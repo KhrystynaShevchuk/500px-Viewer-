761381a9a8734c825d0c1abc08155d4f8333cfcf
KhrystynaShevchuk <khrystyna.shevchuk94@gmail.com>
2016-05-27 11:01:36 +0300

    Created project

---
+++ 500px/500px/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  500px
+//
+//  Created by KhrystynaShevchuk on 5/27/16.
+//  Copyright © 2016 KhrystynaShevchuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
