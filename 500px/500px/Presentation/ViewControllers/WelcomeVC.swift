58adf612bfde9c5688301b46d4f054fd2d5e0f36
KhrystynaShevchuk <khrystyna.shevchuk94@gmail.com>
2016-05-27 16:45:44 +0300

    - config scrollView

--- 500px/500px/Presentation/ViewControllers/WelcomeVC.swift
+++ 500px/500px/Presentation/ViewControllers/WelcomeVC.swift
@@ -10,15 +10,44 @@ import UIKit
 
 class WelcomeVC: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
     
+    let imageViews = [UIImageView]()
+    
+    // MARK: - Lifecycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        setupScrollView()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - Private
+    
+    private func setupScrollView() {
+        let screenSize = UIScreen.mainScreen().bounds
+        let screenWidth = screenSize.width
+        let screenAmount = 3
+        
+        scrollView.backgroundColor = UIColor.blackColor()
+        scrollView.contentSize =  CGSizeMake(screenWidth * CGFloat(screenAmount), screenSize.height)
+        print(scrollView.contentSize.width, scrollView.contentSize.height)
+        
+        for i in 0..<screenAmount {
+            let imageView = UIImageView(image: UIImage(named: "image.png"))
+            let xPosition = screenWidth * CGFloat(i)
+            imageView.frame = CGRect(x: xPosition, y: 0, width: screenWidth, height: screenSize.height)
+            imageView.contentMode = .ScaleAspectFit
+            
+            scrollView.addSubview(imageView)
+            print(xPosition, screenWidth)
+        }
+        
+        print(scrollView.contentSize.width, scrollView.contentSize.height)
     }
 }
 
